57c297a3a48a1b672dc2af0c8904859947dba795
IveriNarozashvili <geobmw91>
2020-04-13 20:31:29 +0300

    update code

--- src/main/java/com/github/IveriNarozashvili/controllers/HomeController.java
+++ src/main/java/com/github/IveriNarozashvili/controllers/HomeController.java
@@ -1,20 +1,38 @@
-
 package com.github.IveriNarozashvili.controllers;
 
+import com.github.IveriNarozashvili.models.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
+    private ArrayList<Book> books = new ArrayList<>();
+
     @GetMapping({"/", "/home"})
     public String home() {
         return "home";
-
     }
+
     @GetMapping("/about")
-    public  String about(){
+    public String about() {
         return "about";
     }
-}
 
+    @GetMapping("/hello")
+    public String hello(Model model, @RequestParam String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/books")
+    public String addBook(Model model, @RequestParam(required = false) String name) {
+        if (name != null) books.add(new Book(name));
+        model.addAttribute("books", books);
+        return "books";
+    }
+}
