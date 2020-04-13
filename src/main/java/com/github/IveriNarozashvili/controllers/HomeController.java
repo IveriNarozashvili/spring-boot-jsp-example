860498c4534fa8774d29698ea17ef7c3aa42027d
IveriNarozashvili <geobmw91>
2020-04-13 19:15:56 +0300

    update code

---
+++ src/main/java/com/github/IveriNarozashvili/controllers/HomeController.java
@@ -0,0 +1,20 @@
+
+package com.github.IveriNarozashvili.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "home";
+
+    }
+    @GetMapping("/about")
+    public  String about(){
+        return "about";
+    }
+}
+
